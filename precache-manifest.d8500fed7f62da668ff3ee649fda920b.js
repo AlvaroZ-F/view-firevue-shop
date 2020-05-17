183d4a2456c2375f090a4ab310ac620e1fa8b303
Álvaro Zambrana Fernández <56718986+AlvaroZ-F@users.noreply.github.com>
2020-05-17 19:02:42 +0200

    Uploadin'

---
+++ precache-manifest.d8500fed7f62da668ff3ee649fda920b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0c8b1fab0bf5c9d17fd",
+    "url": "/css/app.ca76d4dc.css"
+  },
+  {
+    "revision": "8265b9f3a373fbda4f3b",
+    "url": "/css/chunk-vendors.682d984d.css"
+  },
+  {
+    "revision": "b926494de1e1125d7f355d51e8329ca5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f0c8b1fab0bf5c9d17fd",
+    "url": "/js/app.9564e1ae.js"
+  },
+  {
+    "revision": "8265b9f3a373fbda4f3b",
+    "url": "/js/chunk-vendors.d9a8d7d7.js"
+  },
+  {
+    "revision": "e9061cc22aa73d813f51a950eacea012",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
